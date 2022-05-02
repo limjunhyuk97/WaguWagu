29baf473fc0e0f022f0c32caad5c2b609fd89ed0
jun_lim97 <limjunhyuk97@gmail.com>
2022-05-02 20:39:05 +0900

    [feat] WEB 초기 환경 셋팅 : recoil 설치

--- Web/src/index.js
+++ Web/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>
 );
