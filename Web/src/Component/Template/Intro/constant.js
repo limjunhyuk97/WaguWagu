c08bf4f560a7ae55a90427d6665bf44fb9f28b03
jun_lim97 <limjunhyuk97@gmail.com>
2022-06-17 17:32:39 +0900

    [Fix] Intro 페이지 문구 위치 변경 / 이렇게 하면 안됨.

--- Web/src/Component/Template/Intro/constant.js
+++ Web/src/Component/Template/Intro/constant.js
@@ -15,31 +15,31 @@ export const IntroBanner = [
   {
     background: "/assets/pre-login/Pre01.jpg",
     text: [
-      { text: "식사를 위해 기다려야만 하는 시대는", top: 650, left: 84 },
-      { text: "끝났다.", top: 758, left: 849 },
+      { text: "식사를 위해 기다려야만 하는 시대는", top: 450, left: 84 },
+      { text: "끝났다.", top: 558, left: 849 },
     ],
   },
   {
     background: "/assets/pre-login/Pre02.jpg",
     text: [
-      { text: "스마트하게!", top: 602, left: 182 },
-      { text: "내 위치에서 미리 주문하고", top: 717, left: 84 },
-      { text: "방문하여 바로 식사", top: 834, left: 568 },
+      { text: "스마트하게!", top: 402, left: 182 },
+      { text: "내 위치에서 미리 주문하고", top: 517, left: 84 },
+      { text: "방문하여 바로 식사", top: 634, left: 568 },
     ],
   },
   {
     background: "/assets/pre-login/Pre03.jpg",
     text: [
-      { text: "조용한 곳", top: 602, left: 182 },
-      { text: "까페·음식점 방문하지 않고도", top: 717, left: 84 },
-      { text: "자리 확인 가능", top: 836, left: 693 },
+      { text: "조용한 곳", top: 402, left: 182 },
+      { text: "까페·음식점 방문하지 않고도", top: 517, left: 84 },
+      { text: "자리 확인 가능", top: 636, left: 693 },
     ],
   },
   {
     background: "/assets/pre-login/Pre04.jpg",
     text: [
-      { text: "왜 배달, 포장 말고 현장은 없지?", top: 642, left: 172 },
-      { text: "이제 현장 예약은 와구와구", top: 760, left: 84 },
+      { text: "왜 배달, 포장 말고 현장은 없지?", top: 442, left: 172 },
+      { text: "이제 현장 예약은 와구와구", top: 560, left: 84 },
     ],
   },
 ];
